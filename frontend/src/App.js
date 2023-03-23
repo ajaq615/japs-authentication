84069710c3c87dbb789818a6104a19e56b25e9e1
ajquidasol615 <ajquidasol615@gmail.com>
2023-03-23 15:51:20 +0800

    modified:   frontend/node_modules/.cache/.eslintcache 	new file:   frontend/node_modules/.cache/babel-loader/1f3ef7e15dcdd76db4aadafc070661eb95f491edfd695e6c4a0a4caa56acef4f.json 	new file:   frontend/node_modules/.cache/babel-loader/6dec8477cd939e82dc15ae8dc181110f9d0241c9bde4f042ff661781671f1d45.json 	new file:   frontend/node_modules/.cache/babel-loader/7f644ede86b1da839aafe08c391edeab7354d18b125fda04955183bccadb2efc.json 	new file:   frontend/node_modules/.cache/babel-loader/de908df31114dd9630b4ecb6e921bff4739da39304da80d46b30103fe00cba8b.json 	new file:   frontend/node_modules/.cache/babel-loader/e6dfe1245251b1275f98e64f8b675649eb2b7ddcf168734faf7e18c316d2594e.json 	new file:   frontend/node_modules/.cache/default-development/10.pack 	modified:   frontend/node_modules/.cache/default-development/3.pack 	modified:   frontend/node_modules/.cache/default-development/4.pack 	modified:   frontend/node_modules/.cache/default-development/6.pack 	new file:   frontend/node_modules/.cache/default-development/7.pack 	new file:   frontend/node_modules/.cache/default-development/8.pack 	new file:   frontend/node_modules/.cache/default-development/9.pack 	modified:   frontend/node_modules/.cache/default-development/index.pack 	modified:   frontend/node_modules/.cache/default-development/index.pack.old 	modified:   frontend/src/App.js 	modified:   frontend/src/pages/Authentication.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,9 @@ import EventsRootLayout from './pages/EventsRoot';
 import HomePage from './pages/Home';
 import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
-import { action as manipulateEventAction } from './components/EventForm';
+import { action, action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage from './pages/Authentication';
+import AuthenticationPage, {action as authAction} from './pages/Authentication';
 
 const router = createBrowserRouter([
   {
@@ -24,7 +24,8 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       {
         path: 'auth',
-        element: <AuthenticationPage />
+        element: <AuthenticationPage />,
+        action: authAction,
       },
       {
         path: 'events',
